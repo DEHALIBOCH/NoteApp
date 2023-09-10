204502441cd04ebcf94e43f290a01f9f3ad977a7
Marat <almabekov.marat@gmail.com>
2023-09-10 12:56:54 +0600

    Утилитарные классы для описания сортировки списка заметок

---
+++ app/src/main/java/com/example/noteapp/feature_note/domain/util/NoteOrder.kt
@@ -0,0 +1,10 @@
+package com.example.noteapp.feature_note.domain.util
+
+sealed class NoteOrder(val orderType: OrderType) {
+
+    class Title(orderType: OrderType) : NoteOrder(orderType)
+
+    class Date(orderType: OrderType) : NoteOrder(orderType)
+
+    class Color(orderType: OrderType) : NoteOrder(orderType)
+}
